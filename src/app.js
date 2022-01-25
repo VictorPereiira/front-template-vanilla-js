7743c4471ddfe991c8175b77ea41edec372422b5
victorpereiira <victor.psa02@gmail.com>
2022-01-25 20:04:58 -0300

    start template

---
+++ src/app.js
@@ -0,0 +1,7 @@
+function App() {
+    return /*html*/ `
+        <div id="root">Home Page</div>
+    `
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+import './styles/app.css';
+import App from './app.js';
+
+
+document.body.insertAdjacentHTML('afterbegin', App())
